75e6748e3cd04289636ff1c15a109afffa336837
Гетман Максим <m.l.hetman@student.khai.edu>
2025-07-08 02:10:51 +0300

    основний клас

---
+++ BaseShape.cs
@@ -0,0 +1,21 @@
+﻿public class BaseShape
+{
+    public double Side1 { get; set; }
+    public double Height { get; set; }
+
+    public BaseShape(double side1, double height)
+    {
+        Side1 = side1;
+        Height = height;
+    }
+
+    public virtual double GetArea()
+    {
+        return Side1 * Height;
+    }
+
+    public virtual string GetInfo()
+    {
+        return $"Сторона1 = {Side1}, Висота = {Height}";
+    }
+}
