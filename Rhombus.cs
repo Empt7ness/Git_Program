82f865676360bcdde4fb8c5fdbf26fcaf58a49c9
Гетман Максим <m.l.hetman@student.khai.edu>
2025-07-08 02:26:46 +0300

    клас трапезойїда та ромба

---
+++ Rhombus.cs
@@ -0,0 +1,9 @@
+﻿public class Rhombus : BaseShape
+{
+    public Rhombus(double side1, double height) : base(side1, height){}
+
+    public override string GetInfo()
+    {
+        return $"Ромб: Сторона = {Side1}, Висота = {Height}";
+    }
+}
---
+++ Trapezoid.cs
@@ -0,0 +1,19 @@
+﻿public class Trapezoid : BaseShape
+{
+    public double Side2 { get; set; }
+
+    public Trapezoid(double side1, double side2, double height) : base(side1, height)
+    {
+        Side2 = side2;
+    }
+
+    public override double GetArea()
+    {
+        return (Side1 + Side2) * Height / 2;
+    }
+
+    public override string GetInfo()
+    {
+        return $"Трапеція: Сторона1 = {Side1}, Сторона2 = {Side2}, Висота = {Height}";
+    }
+}
